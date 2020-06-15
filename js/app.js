d1b00e2b0bfa244ecf7dbbbdca502291fc63b4c8
Ale <ma.salazarsanchez@gmail.com>
2020-06-14 21:55:50 -0400

    Validating form fields

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const ui = new Interfaz();
+
+
+// Leer el formulario
+const formulario = document.querySelector('#formulario');
+// eventlistener
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // leer la moneda seleccionada
+  const monedaSelect = document.querySelector('#moneda');
+  const monedaSeleccionada = monedaSelect.options[monedaSelect.selectedIndex].value;
+
+  // leer la criptomoneda seleccionada
+  const criptoMonedaSelect = document.querySelector('#criptomoneda');
+  const criptoMonedaSeleccionada = criptoMonedaSelect.options[criptoMonedaSelect.selectedIndex].value;
+
+  // comprobar que ambos campos tengan algo seleccionado
+  if(monedaSeleccionada === '' || criptoMonedaSeleccionada === ''){
+    // Arrojar una alerta de error
+    ui.mostrarMensaje('Ambos campos son obligatorios', 'alert bg-danger text-center');
+  }else{
+    // todo bien, consultar la api
+  }
+})
